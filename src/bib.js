18e8c111d0a9bb18da3571e0380b4504ef5c15a0
Coombs,Karen <coombsk@oclc.org>
2018-03-12 22:07:08 -0500

    Bib class and tests

---
+++ src/bib.js
@@ -0,0 +1,94 @@
+const axios = require("axios");
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+const marc4js = require('marc4js');
+const S = require('string');
+
+const serviceUrl = 'https://worldcat.org/bib/data/';
+
+const Error = require('../src/Error');
+
+module.exports = class Bib {
+    constructor(record) {
+	    this.record = record;
+    }
+    
+    getID(){
+    	var field_001 = this.record.controlFields.filter(controlField => controlField.tag === '001')[0].data
+	    if (isNaN(field_001)) {
+	    	this.id = field_001.slice(3); 
+	    } else {
+	    	this.id = field_001;
+	    }
+	    return this.id
+    }
+    
+    getOCLCNumber(){
+    	return this.record.controlFields.filter(controlField => controlField.tag === '001')[0].data;
+    }
+
+    getTitle(){
+		var title = this.record.dataFields.filter(dataField => dataField.tag == "24.").findSubfield('a').data;
+		if (this.record.dataFields.filter(dataField => dataField.tag == '24.').findSubfield('b').data){
+			title += this.record.dataFields.filter(dataField => dataField.tag == '24.').findSubfield('b').data;
+		}
+		title = S(title).chompRight(" /").s;
+		return title;
+    }
+    
+    getAuthor(){
+    	var author;
+		if (this.record.dataFields.filter(dataField => dataField.tag === '100')) {
+			author = this.record.dataFields.filter(dataField => dataField.tag === "100")[0].findSubfield('a').data;
+		} else if (this.record.dataFields.filter(dataField => dataField.tag === '110')) {
+			author = this.record.dataFields.filter(dataField => dataField.tag === "110")[0].findSubfield('a').data;
+		} else if (this.record.dataFields.filter(dataField => dataField.tag === '111')){
+			author = this.record.dataFields.filter(dataField => dataField.tag === "111")[0].findSubfield('a').data;
+		} else if (this.record.dataFields.filter(dataField => dataField.tag === '700')) {
+			author = this.record.dataFields.filter(dataField => dataField.tag === "700")[0].findSubfield('a').data;
+		} else if (this.record.dataFields.filter(dataField => dataField.tag === '710')) {
+			author = this.record.dataFields.filter(dataField => dataField.tag === '710')[0].findSubfield('a').data;
+		} else if (this.record.dataFields.filter(dataField => dataField.tag === '711')) {
+			author = this.record.dataFields.filter(dataField => dataField.tag === "711")[0].findSubfield('a').data;
+		} else {
+			author = "";
+		}
+		author = S(author).chompRight(',').s;
+		author= S(author).chompRight('.').s;
+
+		return author;
+    }
+    
+    getRecord(){
+    	return this.record;
+    }
+    
+    
+    static find(oclcnumber, accessToken) {
+    	var config = {
+    			  headers: {
+    				  'Authorization': 'Bearer ' + accessToken,
+    				  'Accept': 'application/atom+xml;content="application/vnd.oclc.marc21+xml"',
+    				  'User-Agent': 'node.js KAC client'
+    			  }
+    			};
+    	
+    	var url = serviceUrl + oclcnumber;
+        return new Promise(function (resolve, reject) {
+            axios.get(url, null, config)
+          		.then(response => {
+          			// parse out the MARC Record
+        			let doc = new dom().parseFromString(response.data);
+        			select = xpath.useNamespaces({"atom": "http://www.w3.org/2005/Atom", "rb": "http://worldcat.org/rb"});
+        			let record = select('//atom:content/rb:response', doc)
+          			marc4js.parse(record, {fromFormat: 'marcxml'}, function(err, records) {
+          				resolve(new Bib(records[0]));
+          			});
+          	    		    	
+          	    })
+          		.catch (error => {
+          			reject(new Error(error.response.status, error.response.data));
+          		});
+        });
+    }
+};
---
+++ test/bib.test.js
@@ -0,0 +1,67 @@
+const expect = require('chai').expect;
+const moxios = require('moxios');
+const fs = require('fs');
+const xpath = require('xpath');
+const dom = require('xmldom').DOMParser;
+const marc4js = require('marc4js');
+
+const Bib = require('../src/Bib');
+const bib_response = fs.readFileSync(require('path').resolve(__dirname, './mocks/bibResponse.xml')).toString();
+
+describe('Create Bib test', () => {
+	var bib;
+	  before(() => {
+			let doc = new dom().parseFromString(bib_response);
+			select = xpath.useNamespaces({"atom": "http://www.w3.org/2005/Atom", "rb": "http://worldcat.org/rb"});
+			let record = select('//atom:content/rb:response', doc)
+  			marc4js.parse(record, {fromFormat: 'marcxml'}, function(err, records) {
+  				bib = new Bib(records[0]);
+  			});
+		  });
+	  
+	  it('Creates an bib object', () => {
+		  expect(bib).to.be.an.instanceof(Bib);
+	  });
+	  
+	  it('Sets the bib properties', () => {
+        //expect(bib.record).to.be.an.instanceof(Record);
+	  });
+	  
+	  it('Has functioning getters', () => {
+        expect(bib.getID()).to.equal('70775700')
+        //expect(bib.getRecord()).to.be.an.instanceof(Record);
+        expect(bib.getOCLCNumber()).to.equal('ocm70775700');
+        //expect(bib.getTitle()).to.equal("Dogs and cats");
+        expect(bib.getAuthor()).to.equal("Jenkins, Steve");
+	  });
+	  
+	});
+
+describe('Find Bib tests', () => {
+  beforeEach(() => {
+	  moxios.install();
+  });
+  
+  afterEach(() => {
+	  moxios.uninstall();
+  });
+
+  it('Get Bib by oclc number', () => {
+      moxios.stubRequest('https://worldcat.org/bib/data/70775700', {
+          status: 200,
+          responseText: bib_response
+        });  
+    return Bib.find('70775700', 'tk_12345')
+      .then(response => {
+        //expect an Bib object back
+    	expect(response).to.be.an.instanceof(Bib);
+
+        expect(response.getID()).to.equal('70775700')
+        //expect(response.getRecord()).to.be.an.instanceof(Record);
+        expect(response.getOCLCNumber()).to.equal('ocm70775700');
+        //expect(response.getTitle()).to.equal("Dogs and cats");
+        expect(response.getAuthor()).to.equal("Jenkins, Steve");
+        
+      });
+  });
+});
