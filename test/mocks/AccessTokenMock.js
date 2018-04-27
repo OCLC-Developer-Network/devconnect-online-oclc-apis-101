a2953a3224c9ca0f94bc4cff4c8589bd77e313a2
Coombs,Karen <coombsk@oclc.org>
2018-04-27 15:54:46 -0500

    Fixed AccessTokenMock

--- test/mocks/AccessTokenMock.js
+++ test/mocks/AccessTokenMock.js
@@ -1,12 +1,11 @@
 const nodeauth = require("nodeauth");
 
 module.exports = Object.assign(new nodeauth.AccessToken, {
-	  grantType: 'authorization_code',
+	  grantType: 'client_credentials',
 	  wskey: {
 	     key: 'key',
 	     secret: 'secret',
-	     services: [ 'SCIM:read_self', 'refresh_token' ],
-	     redirectUri: 'http://localhost:8000/myaccount',
+	     services: [ 'WorldCatMetadataAPI'],
 	     user: {
 	        principalID: '1671151d-ac48-4b4d-a204-c858c3bf5d86',
 	        principalIDNS: 'urn:oclc:platform:128807',
@@ -21,16 +20,10 @@ module.exports = Object.assign(new nodeauth.AccessToken, {
 	     authenticatingInstitutionId: '128807' },
 	  contextInstitutionId: '128807',
 	  authenticatingInstitutionId: 128807,
-	  redirectUri: 'http://localhost:8000/myaccount',
-	  code: 'auth_12345678',
-	  refreshToken: {
-	     refreshToken: 'rt_123456',
-	     expiresIn: 604799,
-	     expiresAt: '2028-04-24 21:06:00Z' },
 	  accessTokenString: 'tk_123456',
 	  expiresAt: '2028-04-17 21:21:00Z',
 	  services: undefined,
-	  accessTokenUrl: 'https://authn.sd00.worldcat.org/oauth2/accessToken?grant_type=authorization_code&code=auth_12345678&authenticatingInstitutionId=128807&contextInstitutionId=128807&redirect_uri=http://localhost:8000/myaccount',
+	  accessTokenUrl: 'https://authn.sd00.worldcat.org/oauth2/accessToken?grant_type=client_credentials&&authenticatingInstitutionId=128807&contextInstitutionId=128807&scope=WorldCatMetadataAPI',
 	  errorCode: null,
 	  tokenType: 'bearer',
 	  expiresIn: 899 
