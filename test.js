e912e775a16da50da8b887f271581668856e64f0
Coombs,Karen <coombsk@oclc.org>
2018-04-28 19:32:57 -0500

    Fixed test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const get_config = require("./src/config.js");
 const moxios = require('moxios');
-const access_token = fs.readFileSync(require('path').resolve(__dirname, 'test/mocks/access_token.json')).toString();
+const accessToken = require('./test/mocks/AccessTokenMock');
 const bib_response = fs.readFileSync(require('path').resolve(__dirname, 'test/mocks/bibResponse.xml')).toString();
 const error_response_401 = fs.readFileSync(require('path').resolve(__dirname, 'test/mocks/errorResponse.xml')).toString();
 const error_response_403 = fs.readFileSync(require('path').resolve(__dirname, 'test/mocks/errorResponse_403.xml')).toString();
@@ -10,12 +10,6 @@ const error_response_404 = fs.readFileSync(require('path').resolve(__dirname, 't
 
 
 moxios.install();
-
-// get a valid token
-moxios.stubRequest('https://authn.sd00.worldcat.org/oauth2/accessToken?grant_type=client_credentials&authenticatingInstitutionId=128807&contextInstitutionId=128807&scope=WorldCatMetadataAPI', {
-	  status: 200,
-	  responseText: access_token
-});
 
 moxios.stubRequest('https://worldcat.org/bib/data/70775700', {
     status: 200,
@@ -45,6 +39,7 @@ get_config(environment)
 	.then(function (output){
 		global.config = yaml.load(output);
 		let app = require('./src/server.js');
+		app.set('accessToken', accessToken);
 		let port = process.env.PORT || 8000;
 
 		// Server
