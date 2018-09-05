d211d66fddaf6bdfa930c30d473895e634415ea3
Coombs,Karen <coombsk@oclc.org>
2018-09-05 09:05:51 -0500

    Fixed lambda code to properly call KMS

--- lambda.js
+++ lambda.js
@@ -1,6 +1,19 @@
 const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./src/server');
-const server = awsServerlessExpress.createServer(app)
+const yaml = require('js-yaml');
+const get_config = require("./src/config.js");
+let environment = 'prod';
 
+global.config = "";
 
-module.exports.universal = (event, context) => awsServerlessExpress.proxy(server, event, context);
+module.exports.universal = function(event, context){
+	get_config('prod')
+	.then(function (output){
+		global.config = yaml.load(output);
+		let app = require('./src/server.js');		
+		const server = awsServerlessExpress.createServer(app)
+		return awsServerlessExpress.proxy(server, event, context);
+	})
+	.catch(function (err){
+		throw ('Config failed to load' + err);
+	});
+}
